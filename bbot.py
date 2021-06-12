e35c14e03e54b99a2d66886729a5c18ae6f2fcf7
dhanuj001 <61634217+dhanuj001@users.noreply.github.com>
2021-06-12 12:33:14 +0530

    added a joke command ,sends jokes idk if they are funny or not LOL

--- bbot.py
+++ bbot.py
@@ -1,32 +1,35 @@
 import os
 import discord
+import requests
 from discord.ext import commands
 from discord import Spotify
 
-from dotenv import load_dotenv , find_dotenv
+from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
 
-
-client = commands.Bot(command_prefix="bb",intents=discord.Intents.all())
+client = commands.Bot(command_prefix="bb", intents=discord.Intents.all())
 
 
 @client.event
 async def on_ready():
     print(f"we have logged in as {client.user}")
 
+
 @client.event
 async def on_message(msg):
 
     if client.user.mentioned_in(msg):
 
-        #Bot responds with the current prefix when mentioned in the message
+        # Bot responds with the current prefix when mentioned in the message
         # "#009aff" or 0x009aff if the color used for Beel
-        emb = discord.Embed(description=f"Hi, I currently respond to\n```Prefix: bb```",color=0x009aff)
+        emb = discord.Embed(
+            description=f"Hi, I currently respond to\n```Prefix: bb```", color=0x009aff)
         await msg.channel.send(embed=emb)
 
     await client.process_commands(msg)
+
 
 @client.command()
 async def hello(ctx):
@@ -38,6 +41,7 @@ async def ping(ctx):
 
     await ctx.channel.send(f"Pong {round(client.latency*1000)} ms")
 
+
 @client.command()
 async def spotify(ctx, user: discord.Member = None):
     if user == None:
@@ -47,16 +51,28 @@ async def spotify(ctx, user: discord.Member = None):
         for activity in user.activities:
             if isinstance(activity, Spotify):
                 embed = discord.Embed(
-                    title = f"{user.name}'s Spotify",
-                    description = "Listening to {}".format(activity.title),
-                    color = activity.colour)
+                    title=f"{user.name}'s Spotify",
+                    description="Listening to {}".format(activity.title),
+                    color=activity.colour)
                 embed.set_thumbnail(url=activity.album_cover_url)
                 embed.add_field(name="Artist", value=activity.artist)
                 embed.add_field(name="Album", value=activity.album)
-                embed.set_footer(text="Song started at {}".format(activity.created_at.strftime("%H:%M")))
+                embed.set_footer(text="Song started at {}".format(
+                    activity.created_at.strftime("%H:%M")))
                 await ctx.send(embed=embed)
     else:
         await ctx.send("user is not listening to any song ")
+
+
+@client.command()
+async def joke(ctx):
+    req = requests.get("https://official-joke-api.appspot.com/random_joke")
+    joke = req.json()
+    embed = discord.Embed(
+        title="{}".format(joke['setup']),
+        description="{}".format(joke['punchline']),
+        color=0xFF5733)
+    await ctx.send(embed=embed)
 
 
 client.run(os.getenv('TOKEN'))
