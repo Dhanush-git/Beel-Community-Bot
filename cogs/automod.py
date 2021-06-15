f31d2e3f4036b41b8781fcd8641f24c00c2e78c5
The Professor <akshat.prj@gmail.com>
2021-06-15 10:57:00 +0530

    Update automod.py

--- cogs/automod.py
+++ cogs/automod.py
@@ -2,7 +2,6 @@ import asyncio
 from datetime import datetime
 import discord
 from discord.ext import commands
-import json
 from discord import NotFound, Object
 from typing import Optional
 import datetime
@@ -32,73 +31,134 @@ class BannedUser(Converter):
                 raise BadArgument
 
 
+async def kick_member(ctx, member, reason):
+    if ctx.guild.me.top_role.position < member.top_role.position or member == ctx.guild.owner:
+        await ctx.send("I can't Kick Owner/Moderators/Staff.")
+        return
+    # with open("./json/config.json", "r") as json_file:
+    #     config = json.load(json_file)
+    # try:
+    #     logchanel_id = config[("Guild_ID_" + str(ctx.guild.id))
+    #                           ]["Configuration"]["Logchannel"]
+    #     log_channel = self.client.get_channel(id=logchanel_id)
+    # except KeyError:
+    #     log_channel = ctx.channel
+    await member.send(f"You got Kicked from server **{ctx.guild.name}** by **{ctx.author.name}** for reason: `{reason}`")
+    await member.kick(reason=reason)
+    await ctx.send(f" Member ID : ||{member.id}||")
+    ban_embed = discord.Embed(title="Member Kicked!", colour=warning_color)
+    ban_embed.add_field(name="User", value=member.name)
+    ban_embed.add_field(name="Kicked by", value=ctx.author.name)
+    ban_embed.add_field(name="Reason", value=reason)
+    ban_embed.timestamp = datetime.datetime.utcnow()
+    await ctx.send(embed=ban_embed)
+
+
+async def ban_member(ctx, member, reason):
+    if ctx.guild.me.top_role.position < member.top_role.position or member == ctx.guild.owner:
+        await ctx.send("I can't ban Owner/Moderators/Staff.")
+        return
+    # with open("./json/config.json", "r") as json_file:
+    #     config = json.load(json_file)
+    # try:
+    #     logchanel_id = config[("Guild_ID_" + str(ctx.guild.id))
+    #                           ]["Configuration"]["Logchannel"]
+    #     log_channel = self.client.get_channel(id=logchanel_id)
+    # except KeyError:
+    #     log_channel = ctx.channel
+    await member.send(f"You got banned from server **{ctx.guild.name}** by **{ctx.author.name}** for reason: `{reason}`")
+    await member.ban(reason=reason)
+    await ctx.send(f" Member ID : ||{member.id}||")
+    ban_embed = discord.Embed(title="Member banned", colour=warning_color)
+    ban_embed.add_field(name="User", value=member.name)
+    ban_embed.add_field(name="Banned by", value=ctx.author.name)
+    ban_embed.add_field(name="Reason", value=reason)
+    ban_embed.timestamp = datetime.datetime.utcnow()
+    await ctx.send(embed=ban_embed)
+
+
+async def unban_member(ctx, members, reason):
+    for member in members:
+        # with open("./json/config.json", "r") as json_file:
+        #     config = json.load(json_file)
+        # try:
+        #     logchanel_id = config[(
+        #         "Guild_ID_" + str(ctx.guild.id))]["Configuration"]["Logchannel"]
+        #     log_channel = self.client.get_channel(id=logchanel_id)
+        # except KeyError:
+        #     log_channel = ctx.channel
+
+        await ctx.guild.unban(user=member, reason=reason)
+        await ctx.send(f" Member ID : ||{member.id}||")
+        ban_embed = discord.Embed(
+            title="Member Unbanned", colour=warning_color)
+        ban_embed.add_field(name="User", value=member.name)
+        ban_embed.add_field(name="Reason", value=reason)
+        ban_embed.timestamp = datetime.datetime.utcnow()
+        await ctx.send(embed=ban_embed)
+
+
+async def unmute_members(ctx, member, *, reason="Mute time expired."):
+    # with open("./json/config.json", "r") as json_file:
+    #     config = json.load(json_file)
+    # # try:
+    # #     logchanel_id = config[("Guild_ID_" + str(ctx.guild.id))
+    # #                           ]["Configuration"]["Logchannel"]
+    # #     log_channel = self.client.get_channel(id=logchanel_id)
+    # # except KeyError:
+    #     log_channel = ctx.channel  # sou
+    # with open("./json/mute.json") as f:
+    #     mute_member = json.load(f)
+    mute_role = discord.utils.get(ctx.guild.roles, name="Muted")
+    if mute_role in member.roles:
+
+        # role_ids = mute_member[str(ctx.guild.id)][str(member.id)]['role_ids']
+        # roles = [ctx.guild.get_role(int(id_))
+        #          for id_ in role_ids.split(",") if len(id_)]
+        await member.remove_roles(mute_role)
+        embed = discord.Embed(title="Member unmuted",
+                              colour=warning_color,
+                              timestamp=datetime.datetime.utcnow())
+
+        embed.set_thumbnail(url=member.avatar_url)
+
+        fields = [("Member", member.mention, False),
+                  ("Reason", reason, False)]
+
+        for name, value, inline in fields:
+            embed.add_field(name=name, value=value, inline=inline)
+        await ctx.send(embed=embed)
+        # del mute_member[str(ctx.guild.id)][str(member.id)]
+    else:
+        await ctx.send("Member is not muted")
+    # with open("./json/mute.json", "w") as f:
+    #     json.dump(mute_member, f, indent=4)
+
+
 class Automod(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    async def kick_member(self, ctx, member, reason):
-        if ctx.guild.me.top_role.position < member.top_role.position or member == ctx.guild.owner:
-            await ctx.send("I can't ban Owner/Modeartors/Staff.")
-            return
-        # with open("./json/config.json", "r") as json_file:
-        #     config = json.load(json_file)
-        # try:
-        #     logchanel_id = config[("Guild_ID_" + str(ctx.guild.id))
-        #                           ]["Configuration"]["Logchannel"]
-        #     log_channel = self.client.get_channel(id=logchanel_id)
-        # except KeyError:
-        #     log_channel = ctx.channel
-        await member.send(f"You got Kicked from server **{ctx.guild.name}** by **{ctx.author.name}** for reason: `{reason}`")
-        await member.kick(reason=reason)
-        await ctx.send(f" Member ID : ||{member.id}||")
-        ban_embed = discord.Embed(title="Member Kicked!", colour=warning_color)
-        ban_embed.add_field(name="User", value=member.name)
-        ban_embed.add_field(name="Kicked by", value=ctx.author.name)
-        ban_embed.add_field(name="Reason", value=reason)
-        ban_embed.timestamp = datetime.datetime.utcnow()
-        await ctx.send(embed=ban_embed)
-
     @commands.command()
     @commands.has_guild_permissions(kick_members=True)
     async def _kick(self, ctx, member: discord.Member, *, reason: Optional[str] = "No reason provided"):
-        self.kick_member(ctx, member, reason)
+        await kick_member(ctx, member, reason)
+
     @_kick.error
     async def kick_error(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
             embed = discord.Embed(
-                description="You are missing `ban_member` permission for above action.", colour=warning_color)
+                description="You are missing `Kick_member` permission for above action.", colour=warning_color)
             await ctx.send(embed=embed)
         elif isinstance(error, MemberNotFound):
             embed = discord.Embed(
-                description="Can't ban user who is not in server.", colour=warning_color)
+                description="Can't kick user who is not in server.", colour=warning_color)
             await ctx.send(embed=embed)
-
-    async def ban_member(self, ctx, member, reason):
-        if ctx.guild.me.top_role.position < member.top_role.position or member == ctx.guild.owner:
-            await ctx.send("I can't ban Owner/Modeartors/Staff.")
-            return
-        # with open("./json/config.json", "r") as json_file:
-        #     config = json.load(json_file)
-        # try:
-        #     logchanel_id = config[("Guild_ID_" + str(ctx.guild.id))
-        #                           ]["Configuration"]["Logchannel"]
-        #     log_channel = self.client.get_channel(id=logchanel_id)
-        # except KeyError:
-        #     log_channel = ctx.channel
-        await member.send(f"You got banned from server **{ctx.guild.name}** by **{ctx.author.name}** for reason: `{reason}`")
-        await member.ban(reason=reason)
-        await ctx.send(f" Member ID : ||{member.id}||")
-        ban_embed = discord.Embed(title="Member banned", colour=warning_color)
-        ban_embed.add_field(name="User", value=member.name)
-        ban_embed.add_field(name="Banned by", value=ctx.author.name)
-        ban_embed.add_field(name="Reason", value=reason)
-        ban_embed.timestamp = datetime.datetime.utcnow()
-        await ctx.send(embed=ban_embed)
 
     @commands.command()
     @commands.has_guild_permissions(ban_members=True)
     async def ban(self, ctx, member: discord.Member, *, reason: Optional[str] = "No reason provided"):
-        await self.ban_member(ctx, member, reason=reason)
+        await ban_member(ctx, member, reason=reason)
 
     @ban.error
     async def ban_error(self, ctx, error):
@@ -111,30 +171,36 @@ class Automod(commands.Cog):
                 description="Can't ban user who is not in server.", colour=warning_color)
             await ctx.send(embed=embed)
 
-    async def unban_member(self, ctx, members, reason):
-        for member in members:
-            # with open("./json/config.json", "r") as json_file:
-            #     config = json.load(json_file)
-            # try:
-            #     logchanel_id = config[(
-            #         "Guild_ID_" + str(ctx.guild.id))]["Configuration"]["Logchannel"]
-            #     log_channel = self.client.get_channel(id=logchanel_id)
-            # except KeyError:
-            #     log_channel = ctx.channel
+    @commands.command(aliases=['clearuser'], description="Clear messages of particular user in a channel")
+    @commands.guild_only()
+    @commands.has_permissions(manage_messages=True)
+    async def purgeuser(self, ctx, user: discord.Member = None, num_messages: Optional[int] = 100):
+        user = user or ctx.author
+        channel = ctx.message.channel
+        if ctx.guild.me.top_role < user.top_role or ctx.message.author.top_role < user.top_role:
+            return await ctx.send("Sorry!, I can't clear messages of Owner/staff/moderators.")
 
-            await ctx.guild.unban(user=member, reason=reason)
-            await ctx.send(f" Member ID : ||{member.id}||")
-            ban_embed = discord.Embed(
-                title="Member Unbanned", colour=warning_color)
-            ban_embed.add_field(name="User", value=member.name)
-            ban_embed.add_field(name="Reason", value=reason)
-            ban_embed.timestamp = datetime.datetime.utcnow()
-            await ctx.send(embed=ban_embed)
+        def check(msg):
+            return msg.author.id == user.id
+
+        await ctx.message.delete()
+        await channel.purge(limit=num_messages, check=check, before=None)
+
+    @commands.command(name='clear', help='Using this command you can clear messages in any channel.')
+    @commands.guild_only()
+    @commands.has_permissions(administrator=True)
+    async def clear(self, ctx, amount1: int = 10):
+        channel = ctx.message.channel
+        if 0 < amount1 <= 100:
+            await channel.purge(limit=amount1)
+
+        else:
+            await ctx.send("Limit provided is not in range.")
 
     @commands.command()
     @commands.has_guild_permissions(ban_members=True)
     async def unban(self, ctx, member: Greedy[BannedUser], *, reason: Optional[str] = "No reason provided"):
-        await self.unban_member(ctx, member, reason)
+        await unban_member(ctx, member, reason)
 
     @unban.error
     async def unban_error(self, ctx, error):
@@ -155,39 +221,19 @@ class Automod(commands.Cog):
         #                           ]["Configuration"]["Logchannel"]
         #     log_channel = self.client.get_channel(id=logchanel_id)
         # except KeyError:
-        #     log_channel = ctx.channel  # sourcery no-metrics
+        #     log_channel = ctx.channel  # sorcery no-metrics
         # json_file = open("./json/mute.json", "r")
         # mute_members = json.load(json_file)
-        # try:
-        #     # Gets the numbers from the time argument, start to -1
-        #     seconds = time[:-1]
-        #     duration = time[-1]  # Gets the timed maniulation, s, m, h, d
-        #     if duration == "s":
-        #         seconds = seconds * 1
-        #     elif duration == "m":
-        #         seconds = seconds * 60
-        #     elif duration == "h":
-        #         seconds = seconds * 60 * 60
-        #     elif duration == "d":
-        #         seconds = seconds * 86400
-        #     else:
-        #         await ctx.send("Invalid duration input")
-        #         return
-        # except Exception as e:
-        #     print(e)
-        #     await ctx.send("Invalid time input")
-        #     return
-
         unmutes = []
-        admin_id = self.client.id if ctx.author.id == member.id else ctx.author.id
+        admin_id = self.client.user.id if ctx.author.id == member.id else ctx.author.id
         mute_role = discord.utils.get(ctx.guild.roles, name="Muted")
         if mute_role not in ctx.guild.roles:
-            await ctx.guild.create_role(name="Muted")
+            mute_role = await ctx.guild.create_role(name="Muted")
             for channel in ctx.guild.channels:
                 await channel.set_permissions(mute_role, speak=False, send_messages=False, read_message_history=True, read_messages=False)
 
-        if (mute_role not in member.roles):
-            # end_time = datetime.utcnow() + timedelta(seconds=seconds) if time else None
+        if mute_role not in member.roles:
+            # end_time = datetime.uptown() + timedelta(seconds=seconds) if time else None
             # if str(ctx.guild.id) not in mute_members:
             #     mute_members[str(ctx.guild.id)] = {}
             #     mute_members[str(ctx.guild.id)][str(member.id)] = {'role_ids': role_ids,
@@ -235,7 +281,7 @@ class Automod(commands.Cog):
 
         #         seconds = time[:-1]
         #         seconds = int(seconds)
-        #         duration = time[-1]  # Gets the timed maniulation, s, m, h, d
+        #         duration = time[-1]  # Gets the timed manipulation, s, m, h, d
         #         if duration == "s":
         #             seconds *= 1
         #         elif duration == "m":
@@ -256,48 +302,12 @@ class Automod(commands.Cog):
         if len(unmutes):
             time = time * 60 * 60
             await asyncio.sleep(time)
-            await self.unmute_members(ctx, member)
-
-    async def unmute_members(self, ctx, member, *, reason="Mute time expired."):
-        # with open("./json/config.json", "r") as json_file:
-        #     config = json.load(json_file)
-        # # try:
-        # #     logchanel_id = config[("Guild_ID_" + str(ctx.guild.id))
-        # #                           ]["Configuration"]["Logchannel"]
-        # #     log_channel = self.client.get_channel(id=logchanel_id)
-        # # except KeyError:
-        #     log_channel = ctx.channel  # sou
-        # with open("./json/mute.json") as f:
-        #     mute_member = json.load(f)
-        mute_role = discord.utils.get(ctx.guild.roles, name="Muted")
-        if mute_role in member.roles:
-
-            # role_ids = mute_member[str(ctx.guild.id)][str(member.id)]['role_ids']
-            # roles = [ctx.guild.get_role(int(id_))
-            #          for id_ in role_ids.split(",") if len(id_)]
-            await member.remove_roles(mute_role)
-            embed = discord.Embed(title="Member unmuted",
-                                  colour=warning_color,
-                                  timestamp=datetime.datetime.utcnow())
-
-            embed.set_thumbnail(url=member.avatar_url)
-
-            fields = [("Member", member.mention, False),
-                      ("Reason", reason, False)]
-
-            for name, value, inline in fields:
-                embed.add_field(name=name, value=value, inline=inline)
-            await ctx.send(embed=embed)
-            # del mute_member[str(ctx.guild.id)][str(member.id)]
-        else:
-            await ctx.send("Member is not mutted")
-        # with open("./json/mute.json", "w") as f:
-        #     json.dump(mute_member, f, indent=4)
+            await unmute_members(ctx, member)
 
     @commands.command(name="unmute")
     @commands.has_permissions(manage_roles=True, manage_guild=True)
     async def unmute_command(self, ctx, members: discord.Member, *, reason: Optional[str] = "No reason provided."):
-        await self.unmute_members(ctx, members, reason=reason)
+        await unmute_members(ctx, members, reason=reason)
 
     @mute_command.error
     async def _error(self, ctx, member):
