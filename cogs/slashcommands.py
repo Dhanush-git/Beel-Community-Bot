15202548338997120d9e5b65007071db908bf6a7
The Professor <akshat.prj@gmail.com>
2021-06-15 10:57:04 +0530

    Update slashcommands.py

--- cogs/slashcommands.py
+++ cogs/slashcommands.py
@@ -1,6 +1,4 @@
-from os import name
 import discord
-from discord.activity import Spotify
 from discord.ext import commands
 from discord_slash import cog_ext, SlashContext
 import datetime
@@ -18,7 +16,7 @@ class Slash(commands.Cog):
     @cog_ext.cog_slash(name="ban", description="This command is used to ban user.")
     async def slash_ban(self, ctx: SlashContext, member: discord.Member, *, reason=None):
         if ctx.guild.me.top_role.position < member.top_role.position or member == ctx.guild.owner:
-            await ctx.send("I can't ban Owner/Modeartors/Staff.")
+            await ctx.send("I can't ban Owner/Moderators/Staff.")
             return
         # with open("./json/config.json", "r") as json_file:
         #     config = json.load(json_file)
@@ -30,21 +28,22 @@ class Slash(commands.Cog):
         #     log_channel = ctx.channel
         if reason is None:
             reason = "No reason provided"
-        await member.send(f"You got banned from server **{ctx.guild.name}** by **{ctx.author.name}** for reason: `{reason}`")
+        await member.send(
+            f"You got banned from server **{ctx.guild.name}** by **{ctx.author.name}** for reason: `{reason}`")
         await member.ban(reason=reason)
         ban_embed = discord.Embed(title="Member banned", colour=warning_color)
-        ban_embed.add_field(nmae="Member ID : ||{member.id}||")
-        ban_embed.add_field(name="User", value=member.name)
+        ban_embed.add_field(name="Member ID", value=member.id, inline=False)
+        ban_embed.add_field(name="User", value=member.name, inline=False)
         ban_embed.add_field(
-            name="Banned by", value=ctx.author.name, inllne=True)
-        ban_embed.add_field(name="Reason", value=reason, inllne=True)
+            name="Banned by", value=ctx.author.name, inline=False)
+        ban_embed.add_field(name="Reason", value=reason, inline=False)
         ban_embed.timestamp = datetime.datetime.utcnow()
         await ctx.send(embed=ban_embed)
 
     @cog_ext.cog_slash(name="kick", description="This command is used to kick someone from the server.")
     async def slash_kick(self, ctx: SlashContext, member: discord.Member, *, reason=None):
         if ctx.guild.me.top_role.position < member.top_role.position or member == ctx.guild.owner:
-            await ctx.send("I can't ban Owner/Modeartors/Staff.")
+            await ctx.send("I can't ban Owner/Moderators/Staff.")
             return
         # with open("./json/config.json", "r") as json_file:
         #     config = json.load(json_file)
@@ -56,7 +55,8 @@ class Slash(commands.Cog):
         #     log_channel = ctx.channel
         if reason is None:
             reason = "No reason provided"
-        await member.send(f"You got Kicked from server **{ctx.guild.name}** by **{ctx.author.name}** for reason: `{reason}`")
+        await member.send(
+            f"You got Kicked from server **{ctx.guild.name}** by **{ctx.author.name}** for reason: `{reason}`")
         await member.kick(reason=reason)
         kick_embed = discord.Embed(
             title="Member Kicked!", colour=warning_color)
@@ -69,8 +69,42 @@ class Slash(commands.Cog):
         kick_embed.timestamp = datetime.datetime.utcnow()
         await ctx.send(embed=kick_embed)
 
-    @cog_ext.cog_slash(name="Spotify", description="")
-    async def spotify(self, ctx: SlashContext, user: discord.Member = None):
+    @cog_ext.cog_slash(name="unlock channel")
+    async def slash_lock(self, ctx: SlashContext, role: discord.Role):
+        await ctx.channel.set_permissions(role, send_messages=False, read_messages=True)
+        await ctx.send("Channel locked.", delete_after=10)
+
+    @cog_ext.cog_slash(name="lock channel", description="Locks channel for a role.")
+    async def slash_unlock(self, ctx: SlashContext, role: discord.Role):
+        await ctx.channel.set_permissions(role, send_messages=True, read_messages=True)
+        await ctx.send("Channel unlocked.", delete_after=10)
+
+    @cog_ext.cog_slash(name='clear', description='clears messages in any channel.')
+    async def slash_clear(self, ctx: SlashContext, amount1: int = 10):
+        channel = ctx.message.channel
+        if 0 < amount1 <= 100:
+            await channel.purge(limit=amount1)
+
+        else:
+            await ctx.send("Limit provided is not in range.")
+
+    @cog_ext.cog_slash(name='clearuser', description="Clear messages of particular user in a channel")
+    async def slash_purgeuser(self, ctx: SlashContext, user: discord.Member = None, num_messages: int = None):
+        if num_messages is None:
+            num_messages = 100
+        user = user or ctx.author
+        channel = ctx.message.channel
+        if ctx.guild.me.top_role < user.top_role or ctx.message.author.top_role < user.top_role:
+            return await ctx.send("Sorry!, I can't clear messages of Owner/staff/moderators.")
+
+        def check(msg):
+            return msg.author.id == user.id
+
+        await ctx.message.delete()
+        await channel.purge(limit=num_messages, check=check, before=None)
+
+    @cog_ext.cog_slash(name="Spotify")
+    async def slash_spotify(self, ctx: SlashContext, user: discord.Member = None):
         if user is None:
             user = ctx.author
         if user.activities:
@@ -90,7 +124,7 @@ class Slash(commands.Cog):
             await ctx.send("user is not listening to any song ")
 
     @cog_ext.cog_slash(name="joke", description="Sends joke")
-    async def joke(self, ctx:SlashContext):
+    async def slash_joke(self, ctx: SlashContext):
         req = requests.get("https://official-joke-api.appspot.com/random_joke")
         joke = req.json()
         embed = discord.Embed(
@@ -99,29 +133,16 @@ class Slash(commands.Cog):
             color=0xFF5733)
         await ctx.send(embed=embed)
 
-    @cog_ext.cog_slash(name="insult", description="")
-    async def insult(self, ctx: SlashContext, user: discord.Member = None):
-        if user is None:
-            await ctx.send("No u cant insult yourself,Tag someone")
-        else:
-            req = requests.get(
-                "https://evilinsult.com/generate_insult.php?lang=en&type=json")
-            insult = req.json()
-            embed = discord.Embed(title="{}".format(user.display_name),
-                                  description="{}".format(insult['insult']),
-                                  color=0xFF5733)
-            await ctx.send(embed=embed)
-
     @cog_ext.cog_slash(name="createavatar", description="Creates avatar from random characters")
-    async def createavatar(self, ctx: SlashContext, a=None):
+    async def slash_createavatar(self, ctx: SlashContext, a=None):
         if a is None:
             await ctx.send("please enter a random text to generate your avatar")
         else:
             req = requests.get(
-                f'https://robohash.org/{a}.png?set=set{random.randint(1,3)}')
+                f'https://robohash.org/{a}.png?set=set{random.randint(1, 3)}')
             img = req.url
             embed = discord.Embed(title="your avatar",
-                                  description=f"Here is your random avatar genrated by your text {a}",
+                                  description=f"Here is your random avatar generated by your text {a}",
                                   color=0xFF5733)
             embed.set_image(url=img)
             embed.set_footer(text="created by https://robohash.org/")
@@ -130,9 +151,10 @@ class Slash(commands.Cog):
     # to send avatar of the user
 
     @cog_ext.cog_slash(name="avatar", description="sends user avatar")
-    async def av(self, ctx: SlashContext, user: discord.User = None):
+    async def slash_av(self, ctx: SlashContext, user: discord.User = None):
         if isinstance(user, commands.UserNotFound):
-            await ctx.send("Hmm 🤔, I had a hard time finding the user, you sure that user exits❓ it got a bit chilly right now 👻")
+            await ctx.send(
+                "Hmm 🤔, I had a hard time finding the user, you sure that user exits❓ it got a bit chilly right now 👻")
             return
 
         emb = discord.Embed(title="Avatar")
@@ -146,7 +168,10 @@ class Slash(commands.Cog):
         emb.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
         await ctx.send(embed=emb)
 
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(" <Slash commands loaded>")
+
 
 def setup(client):
     client.add_cog(Slash(client))
-    print(" <Slash commands loaded>")
